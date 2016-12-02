f97e32248ab2cc22a306b8f7a55a9726f66c8675
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2016-12-02 12:17:43 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+
+    words = words.select do |word|
+      sort_letters(anagram) == sort_letters(word)
+    end
+  end
+
+  def sort_letters(word)
+    word.split("").sort.join
+  end
+end
